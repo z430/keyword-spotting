63e87beeb7fd591a6bea91289a37ce064652a368
mays <maystya@gmail.com>
2021-08-19 18:04:43 +0700

    add callback

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 """ Training """
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from models import models
 from utils.dataset import LitKeywordSpotting
 
@@ -26,7 +26,7 @@ def main():
 
     # define the model
     lit_data = LitKeywordSpotting(
-        root_dir=datadir, wanted_words=wanted_words, batch_size=4
+        root_dir=datadir, wanted_words=wanted_words, batch_size=64
     )
     len_classes = lit_data.total_clases
     input_size = lit_data.input_size
@@ -36,12 +36,21 @@ def main():
     wandb_logger = WandbLogger(project="Keyword Spotting", log_model="all")
 
     early_stopping = EarlyStopping("val_loss")
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val/loss",
+        dirpath="my/path/",
+        filename="sample-mnist-epoch{epoch:02d}-val_loss{val/loss:.2f}",
+        auto_insert_metric_name=False,
+        save_top_k=2,
+    )
 
     trainer = pl.Trainer(
         logger=wandb_logger,
-        callbacks=[early_stopping],
+        callbacks=[early_stopping, checkpoint_callback],
         deterministic=True,
         weights_save_path="checkpoints/",
+        gpus=-1,
+        max_epochs=100,
     )
     wandb_logger.watch(net)
     trainer.fit(net, lit_data)
